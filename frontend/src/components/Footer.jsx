7392a0ba73b4e08fc54a50ab28eb507f8444218b
Saurabh Varunkar <128482646+ByteBard7@users.noreply.github.com>
2024-09-02 18:59:14 +0530

    made some changes in  footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,17 @@
+import React from 'react';
+
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800">
-      <div className="flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-        <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-          &copy; 2024 SteamBeam. All rights reserved.
+    <footer className="py-2 bg-black text-white border-t border-gray-800">
+      <div className="container mx-auto flex flex-col items-center justify-center gap-4 md:flex-row md:justify-between md:h-24">
+        <p className="text-sm leading-loose text-center text-gray-400 mx-auto">
+          &copy; {currentYear} SteamBeam. All rights reserved.
         </p>
       </div>
     </footer>
   );
 };
+
 export default Footer;
